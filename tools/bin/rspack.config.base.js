a9160930a2b34c761278f2c45a5febb5052f2aa3
nuintun <nuintun@qq.com>
2025-07-24 16:16:30 +0800

    build: modify progress bar prefix and add icon
    - Remove square brackets from process prefix
    - Add icon to progress bar template

--- tools/bin/rspack.config.base.js
+++ tools/bin/rspack.config.base.js
@@ -112,8 +112,8 @@ export default async mode => {
 
   const process = {
     progressChars: '█▒',
-    prefix: `[${appConfig.name}]`,
-    template: '{prefix:.bold} {bar:25.green/white.dim} ({percent}%) {wide_msg:.dim}'
+    prefix: appConfig.name,
+    template: '<i> [{prefix:.bold}] {bar:25.green/white.dim} ({percent}%) {wide_msg:.dim}'
   };
 
   return {
